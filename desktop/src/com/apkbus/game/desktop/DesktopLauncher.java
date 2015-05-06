03956e74b12a02f986ba32034ff9a4bb41821a59
PotatoGames <zz316344618@163.com>
2015-05-06 11:31:20 +0800

    test

--- desktop/src/com/apkbus/game/desktop/DesktopLauncher.java
+++ desktop/src/com/apkbus/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		
-		config.title = "game";
+		config.title = "game1";
 		
 		new LwjglApplication(new MainGame(), config);
 	}
